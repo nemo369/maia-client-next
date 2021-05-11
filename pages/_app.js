ceaac733393c3ce7340632b4bf0676f922b9cc75
Your username <your_email@gmool.mom>
2021-05-11 18:00:15 +0300

    convert To Lf

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,17 @@
 /* eslint-disable react/prop-types */
 import '../styles/main.scss';
-import '../styles/global.css'
+import '../styles/global.css';
 import Layout from '../components/Layout';
 
-
 function MyApp({ Component, pageProps }) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-
-
 export default MyApp;
-
 
 // export default class MyApp extends App {
 // 	render () {
