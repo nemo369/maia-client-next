4533e2097345825e7e654f31a271f34af4e3811c
Your username <your_email@gmool.mom>
2021-05-12 15:14:42 +0300

    added 5 new pages to dashbord

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,8 +5,20 @@ const NavBar = () => (
   <div className="navbar-container">
     navbar
     <br />
-    <Link href="/Test2">
-      <a>go</a>
+    <Link href="/ProfilePage">
+      <a>Profile page</a>
+    </Link>
+    <br />
+    <Link href="/JobVacancies">
+      <a>Job vacancies</a>
+    </Link>
+    <br />
+    <Link href="/TheCurriculum">
+      <a>The curriculum</a>
+    </Link>
+    <br />
+    <Link href="/professionalism">
+      <a>professionalism</a>
     </Link>
   </div>
 );
---
+++ pages/JobVacancies.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function JobVacancies() {
+  return <div>Job vacancies</div>;
+}
