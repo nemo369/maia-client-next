03ad0978266a9ad594e8dc50af37c335b80071c8
Mickey H <mickey@dicemarketing.co.il>
2021-05-23 12:59:59 +0300

    changed style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,62 +1,38 @@
 module.exports = {
-	future: {
-		removeDeprecatedGapUtilities: true,
-		purgeLayersByDefault: true,
-	},
-	purge: [
-		'./src/components/**/*.js',
-		'./pages/**/*.js',
-	],
-	variants: {},
-	plugins: [
-		require('tailwindcss'),
-		require('precss'),
-		require('autoprefixer')
-	],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['./src/components/**/*.js', './pages/**/*.js'],
+  variants: {},
+  plugins: [require('tailwindcss'), require('precss'), require('autoprefixer')],
+  darkMode: false, // or 'media' or 'class'
 
-	theme: {
-		extend: {
-			spacing: {
-				bbc: '1920px',
-			},
-			
-			textColor: theme => theme('colors'),
-			textColor: {
-				
-
-			},
-
-			backgroundColor: theme => ({
-				...theme('colors'),
-				'mainOrange': '#FB9773',
-				
-			}),
-
-			fontSize: {
-				'topBarFontSize': '25px',
-				
-
-			}
-
-
-
-
-
-
-			//  height: {
-			//   sm: '8px',
-			//   md: '16px',
-			//   lg: '24px',
-			//   xl: '100px',
-			//  }
-		}
-	}
+  theme: {
+    extend: {
+      colors: {
+        orangeMaya: '#FF916A',
+        whiteMaya: '#FFFFFF',
+        blackMaya: '#000000',
+        redMaya: '#F97168',
+        greenMaya: {
+          DEFAULT: '#008B92',
+          light: '#B3DDCF',
+          dark: '#317885',
+          lighter: '#A8DFCE',
+        },
+        blueMaya: {
+          DEFAULT: '#4EA8B5',
+          light: '#45A4C7',
+          dark: '#252464',
+        },
+        greymaya: {
+          DEFAULT: '#999999',
+          light: '#CCCCCC',
+          dark: '#6C6C6C',
+        },
+        yellowMaya: '#FFC960',
+      },
+    },
+  },
 };
