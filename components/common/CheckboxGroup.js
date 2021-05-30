8fae52ecb955adeb19c2307c8880a33ef65d091e
naaman1 <nafrenkel@gmail.com>
2021-05-30 17:26:16 +0300

    fix build

--- components/common/CheckboxGroup.js
+++ components/common/CheckboxGroup.js
@@ -5,20 +5,20 @@ export default function CheckboxGroup() {
     <div className="button-group">
       <div className="button-sec">
         <label className="button-label" htmlFor="r1">
-          הכל
+          <span className="sr-only">הכל</span>
+          <input type="radio" id="r1" name="r-group" />
         </label>
-        <input type="radio" id="r1" name="r-group" />
       </div>
       <div className="button-sec">
-        <input type="radio" id="r2" name="r-group" />
         <label className="button-label" htmlFor="r2">
-          הכי מתאים
+          <input type="radio" id="r2" name="r-group" />
+          <span className="sr-only">הכי מתאים</span>
         </label>
       </div>
       <div className="button-sec">
-        <input type="radio" id="r3" name="r-group" />
         <label className="button-label" htmlFor="r3">
-          הכי חדש
+          <input type="radio" id="r3" name="r-group" />
+          <span className="sr-only">הכי חדש</span>
         </label>
       </div>
     </div>
