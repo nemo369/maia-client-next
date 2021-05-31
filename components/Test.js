9b6b10e0ba770117be4e7f33630967a1ba31b793
Mickey H <mickey@dicemarketing.co.il>
2021-05-31 14:34:30 +0300

    radio button

--- components/Test.js
+++ components/Test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from './common/Button';
 import ToggleSchoolJobs from './common/ToggleSchoolJobs';
 import CheckboxGroup from './common/CheckboxGroup';
+import RadioMaleFemale from './common/RadioMaleFemale';
 import Check from './common/Check';
 import Inputs from './common/Inputs';
 import NumInputs from './common/NumInputs';
@@ -11,7 +12,6 @@ export default function Test() {
   return (
     <div>
       <h1>Buttons</h1>
-      <div>------------------------</div>
       <Button type="button" status="main" name="ראשי" className="h-[50px] w-[250px]" />
       <Button
         type="button"
@@ -37,15 +37,14 @@ export default function Test() {
       <div className="h-3 w-3 my-3">====</div>
       <Toggle />
       <div className="h-3 w-3 my-3">====</div>
+      <div>------------------------</div>
       <h1>Checkbox Group</h1>
-      <div className="h-3 w-3 my-3">====</div>
       <CheckboxGroup />
-      <div className="h-3 w-3 my-3">====</div>
+      <div>------------------------</div>
       <h1>Check</h1>
       <Check className="m-3" />
-      <div className="h-3 w-3 my-3">====</div>
+      <div>------------------------</div>
       <h1>Inputs</h1>
-      <div className="h-3 w-3 my-3">====</div>
       <div className="bg-white-active">
         <Inputs placeholder="רגיל הרשמה" className="h-[50px] w-[300px]" status="main" />
         <div h-3 w-3 mt-3>
@@ -62,8 +61,8 @@ export default function Test() {
         <Inputs placeholder="שגיאה" className="h-[50px] w-[300px]" status="error" />
       </div>
       <div className="h-3 w-3 my-3">====</div>
-      <h1>NumInputs</h1>
       <div>------------------------</div>
+      <h1>NumInputs</h1>
       <NumInputs status="main" />
       <div h-3 w-3 mt-3>
         ====
@@ -73,6 +72,10 @@ export default function Test() {
         ====
       </div>
       <NumInputs status="error" />
+      <div>------------------------</div>
+      <h1>RadioMaleFemale</h1>
+      <RadioMaleFemale />
+      <div>------------------------</div>
     </div>
   );
 }
