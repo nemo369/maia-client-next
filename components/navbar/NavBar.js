2732a4f84071ec776a2efaa842a77d5d81082c1b
Your username <your_email@gmool.mom>
2021-06-08 11:19:57 +0300

    register p

--- components/navbar/NavBar.js
+++ components/navbar/NavBar.js
@@ -3,7 +3,6 @@ import React, { useContext } from 'react';
 import { useRouter } from 'next/router';
 import LightBulb from '../svg/LightBulb';
 import { AppContext } from '../../src/context/state';
-import Logosvg from '../svg/Logo';
 import Information from '../svg/Information';
 import SchoolHat from '../svg/SchoolHat';
 import Briefcase from '../svg/Briefcase';
@@ -13,14 +12,13 @@ import SilverLogo from '../svg/SilverLogo';
 const NavBar = () => {
   const { pathname } = useRouter();
   const { user } = useContext(AppContext);
+  console.log(user);
 
   const links = [
-    { href: '/dashbaord', icon: <ProfilePic /> },
-    { href: '/dashbaord', name: 'ראשי', icon: <LightBulb /> },
+    { href: '/', name: 'ראשי', icon: <LightBulb /> },
     { href: '/professions', name: 'זירת המקצועות', icon: <Information /> },
     { href: '/school', name: 'מאגר הלימודים', icon: <SchoolHat /> },
     { href: '/jobs', name: 'משרות פנויות', icon: <Briefcase /> },
-    { href: '/jobs', icon: <SilverLogo /> },
   ];
 
   const LinkButton = ({ link, isActive }) => (
@@ -41,10 +39,13 @@ const NavBar = () => {
     <div className="nav__wrapper flex bg-green-500">
       <div className="nav__placeholder h-screen w-[150px]" />
       <div className="nav__border rounded-r-lg bg-gr hidden md:block z-20" />
-      <aside className="fixed z-10 w-[115px] right-5 nav flex flex-col h-screen justify-between bg-green-500 items-center">
+      <aside className="fixed z-10 w-[115px] right-5 nav flex  flex-col h-screen justify-around bg-green-500 items-center">
         <div className="nav__profile">
-          <Link href="/ProfilePage">
-            <a>{user?.user_display_name}</a>
+          <Link href="/profile">
+            <a>
+              {user?.user_display_name}
+              <ProfilePic />
+            </a>
           </Link>
         </div>
         <ul className="nav__links flex flex-col items-center justify-center">
@@ -55,7 +56,7 @@ const NavBar = () => {
         <div className="nav__logo">
           <Link href="/">
             <a>
-              <Logosvg />
+              <SilverLogo />
             </a>
           </Link>
         </div>
