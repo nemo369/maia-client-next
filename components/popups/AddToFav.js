ee01817b9d2b5357af43c95ff400baaa41df1d9a
Mickey H <mickey@dicemarketing.co.il>
2021-07-22 17:49:52 +0300

    popup components

---
+++ components/popups/AddToFav.js
@@ -0,0 +1,36 @@
+import { useTranslation } from 'next-i18next';
+import React, { useState } from 'react';
+import PopUp from '../common/PopUp';
+import PagePen from '../svg/PagePen';
+import Button from '../common/Button';
+
+const AddToFav = () => {
+  const [isDone, setIsDone] = useState(false);
+  const { t } = useTranslation('common');
+
+  return (
+    <div>
+      <PopUp defaultOpen>
+        <PopupContent />
+      </PopUp>
+    </div>
+  );
+};
+
+export default AddToFav;
+
+const PopupContent = () => (
+  <div className="flex flex-col items-center justify-center py-16 px-16">
+    <PagePen />
+    <h2 className="text-center text-3xl font-bold ">מקצוע נוסף למועדפים</h2>
+    <div>
+      {t('באזור האישי תוכל לצפות בכל')}
+      <br />
+      המקצועות / המשרות והלימודים
+      <br />
+      שהוספת למועדפים
+    </div>
+    <Button className="h-[50px] w-[240px]" status="secondary" name="לאזור האישי" />
+    <Button className="h-[50px] w-[240px]" status="secondary" name="סגור" />
+  </div>
+);
---
+++ components/popups/NextStepPopUp.js
@@ -0,0 +1,40 @@
+import { useTranslation } from 'next-i18next';
+import React, { useState } from 'react';
+import PopUp from '../common/PopUp';
+import PagePen from '../svg/PagePen';
+import Button from '../common/Button';
+
+const NextStepPopUp = () => {
+  const [isDone, setIsDone] = useState(false);
+  const { t } = useTranslation('common');
+
+  return (
+    <div>
+      <PopUp defaultOpen>
+        <PopupContent />
+      </PopUp>
+    </div>
+  );
+};
+
+export default NextStepPopUp;
+
+const PopupContent = () => (
+  <div className="flex flex-col items-center justify-center py-16 px-16">
+    <PagePen />
+    <h2 className="text-center text-3xl font-bold ">
+      מעבר אל השלב
+      <br />
+      ״מה מעניין אותי״
+    </h2>
+    <div>
+      בשלב זה תועבר אל שאלון הקריירה
+      <br />
+      שיסייע לך לזהות את תחומי העניין
+      <br />
+      התעסוקתיים שלך
+    </div>
+    <Button className="h-[50px] w-[240px]" status="secondary" name="התחל" />
+    <Button className="h-[50px] w-[240px]" status="secondary" name="סגור" />
+  </div>
+);
