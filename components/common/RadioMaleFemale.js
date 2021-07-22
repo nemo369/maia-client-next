48b87ba77e92f95366adda2684c0077eb6023437
Mickey H <mickey@dicemarketing.co.il>
2021-07-22 10:52:15 +0300

    added data to user info

--- components/common/RadioMaleFemale.js
+++ components/common/RadioMaleFemale.js
@@ -1,23 +1,50 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 
-export default function RadioMaleFemale({ onChangeGender, className }) {
+export default function RadioMaleFemale({ className, initialValue }) {
+  const handleSelectCahnge = ({ value }) => {
+    console.log(value.value);
+  };
   return (
-    <div className={`${className} flex inline-block`} onChange={onChangeGender}>
+    <div className={`${className} flex`}>
       <div className="flex">
-        <input type="radio" value="MALE" name="gender" id="male" required />
+        <input
+          type="radio"
+          value="m"
+          name="gender"
+          id="male"
+          onChange={(e) => handleSelectCahnge({ value: e.target })}
+          required
+          defaultChecked={'m' === initialValue}
+        />
         <label className="radio-label ml-3" htmlFor="male">
           זכר
         </label>
       </div>
       <div className="flex">
-        <input type="radio" value="FEMALE" name="gender" id="female" defaultChecked required />
+        <input
+          type="radio"
+          value="f"
+          name="gender"
+          id="female"
+          required
+          onChange={(e) => handleSelectCahnge({ value: e.target })}
+          defaultChecked={'f' === initialValue}
+        />
         <label className="radio-label ml-3" htmlFor="female">
           נקבה
         </label>
       </div>
       <div className="flex">
-        <input type="radio" value="OTHER" name="gender" id="other" defaultChecked required />
+        <input
+          type="radio"
+          value="o"
+          name="gender"
+          id="other"
+          required
+          onChange={(e) => handleSelectCahnge({ value: e.target })}
+          defaultChecked={'o' === initialValue}
+        />
         <label className="radio-label ml-3" htmlFor="other">
           אחר
         </label>
--- components/profile/ProfileDetails.js
+++ components/profile/ProfileDetails.js
@@ -1,33 +1,109 @@
-import React, { useState } from 'react';
+/* eslint-disable prettier/prettier */
+import React, { useState, useContext } from 'react';
 import Inputs from '../common/Inputs';
 import Arrow from '../svg/Arrow';
 import RadioMaleFemale from '../common/RadioMaleFemale';
+import { AppContext } from '../../src/context/state';
+import Check from '../common/Check';
+import Tooltip from '../common/Tooltip';
 
 export default function ProfileDetails() {
+  const { profile } = useContext(AppContext);
   const [isOpen, setIsOpen] = useState(false);
-
+  const [isDisabled, setIsDiabled] = useState(true);
+  console.log(profile);
   const toggleInfo = () => {
     setIsOpen(!isOpen);
   };
+
+  const editInfo = () => {
+    setIsDiabled(!isDisabled);
+  };
+
+  const tooltipSendedJobs = '<span>סגירת מצב ״מחפש עבודה״ תציג אותך במצב לא פעיל אצל<br /> המעסקים שאליהם שלחת בקשה והם לא יכולו לראות את <br /> פרטיך האישיים.</span>';
+
   return (
     <div>
-      <div className="flex justify-between my-[15px] mx-[30px]">
+      <div className="flex justify-between my-[10px] mx-[30px]">
         <div className="text-gray text-[19px] font-bold">פרטים אישיים</div>
         <button className="focus:outline-none" onClick={toggleInfo} type="button">
-          <Arrow />
+          <div className={isOpen && 'rotate-180'}>
+            <Arrow />
+          </div>
         </button>
       </div>
       {isOpen && (
         <div className="absolute bg-white z-10 w-full">
           <div className="flex flex-col items-center">
-            <Inputs type="text" status="main" className="profile-inputs" placeholder="שם" />
-            <Inputs type="text" status="main" className="profile-inputs" placeholder="טלפון" />
-            <Inputs type="text" status="main" className="profile-inputs" placeholder="מייל" />
-            <Inputs type="text" status="main" className="profile-inputs" placeholder="עיר מגורים" />
-            <Inputs type="text" status="main" className="profile-inputs" placeholder="שפת אם" />
+            <Inputs
+              type="text"
+              status="main"
+              className={`profile-inputs ${isDisabled && 'text-[#717171]'}`}
+              placeholder="שם"
+              value={profile.display_name}
+              disabled={isDisabled}
+            />
+            <Inputs
+              type="text"
+              status="main"
+              className="profile-inputs text-[#717171]"
+              placeholder="טלפון"
+              value={profile.cellphone}
+              disabled
+            />
+            <Inputs
+              type="text"
+              status="main"
+              className="profile-inputs text-[#717171]"
+              placeholder="מייל"
+              value={profile.user_email}
+              disabled
+            />
+            <Inputs
+              type="text"
+              status="main"
+              className={`profile-inputs ${isDisabled && 'text-[#717171]'}`}
+              placeholder="עיר מגורים"
+              value={JSON.parse(profile.city).name}
+              disabled={isDisabled}
+            />
+            <Inputs
+              type="text"
+              status="main"
+              className={`profile-inputs ${isDisabled && 'text-[#717171]'}`}
+              placeholder="שפת אם"
+              value="עברית"
+              disabled={isDisabled}
+            />
             <div className="flex items-center justify-between w-[365px]">
-              <Inputs type="text" status="main" placeholder="גיל" className="profile-inputs-age" />
-              <RadioMaleFemale />
+              <Inputs
+                type="text"
+                status="main"
+                placeholder="גיל"
+                className={`profile-inputs-age ${isDisabled && 'text-[#717171]'}`}
+                value={profile.age}
+                disabled={isDisabled}
+              />
+              <RadioMaleFemale initialValue={profile.gender} />
+            </div>
+            <div className="flex justify-between w-[345px] my-[5px]">
+              <Check content="אני מאשר/ת ליועץ/ת לצפות בפרטים שלי" />
+              <Tooltip
+                trigger={
+                  <div className="relative smallpop w-4 h-4 border-solid border-[#666666] border-[1px] rounded-full font-small opacity-50  text-[#666666] text-xs mr-4 hover:bg-[#3C91A0] hover:opacity-100 hover:text-white inline-block text-center">
+                    ?
+                  </div>
+                }
+              >
+                <div dangerouslySetInnerHTML={{ __html: tooltipSendedJobs }} />
+              </Tooltip>
+            </div>
+            <div className="dash w-[365px] border-b-[1px] border-dashed border-[#979797] opacity-20 h-1" />
+            <div className="my-[15px] flex w-[365px] justify-between">
+              <div className="text-[#666666] text-[18px]">
+                עריכת פרטי שאלון אוטוביוגרפיה
+              </div>
+              <button onClick={editInfo} type="button">ICON</button>
             </div>
           </div>
         </div>
