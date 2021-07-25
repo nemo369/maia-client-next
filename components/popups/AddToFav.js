67b3f140789759b2457b1aee65c599cc343fac35
Mickey H <mickey@dicemarketing.co.il>
2021-07-25 11:45:46 +0300

    popup style

--- components/popups/AddToFav.js
+++ components/popups/AddToFav.js
@@ -1,36 +1,42 @@
 import { useTranslation } from 'next-i18next';
-import React, { useState } from 'react';
+import React from 'react';
+import Link from 'next/link';
 import PopUp from '../common/PopUp';
 import AddFav from '../svg/AddFav';
 import Button from '../common/Button';
 
-const AddToFav = () => {
-  const [isDone, setIsDone] = useState(false);
-  const { t } = useTranslation('common');
-
-  return (
-    <div>
-      <PopUp defaultOpen>
-        <PopupContent />
-      </PopUp>
-    </div>
-  );
-};
+const AddToFav = () => (
+  <div>
+    <PopUp defaultOpen>
+      <PopupContent />
+    </PopUp>
+  </div>
+);
 
 export default AddToFav;
 
-const PopupContent = () => (
-  <div className="flex flex-col items-center justify-center py-16 px-16">
-    <AddFav />
-    <h2 className="text-center text-3xl font-bold ">מקצוע נוסף למועדפים</h2>
-    <div>
-      {t('באזור האישי תוכל לצפות בכל')}
-      <br />
-      המקצועות / המשרות והלימודים
-      <br />
-      שהוספת למועדפים
+const PopupContent = () => {
+  const { t } = useTranslation('common');
+  return (
+    <div className="flex flex-col items-center justify-center px-[65px]">
+      <AddFav />
+      <h2 className="text-center text-3xl mt-[11px] font-bold ">מקצוע נוסף למועדפים</h2>
+      <div className="text-center text-[20px] mt-[50px] leading-[30px]">
+        {t('באזור האישי תוכל לצפות בכל')}
+        <br />
+        המקצועות / המשרות והלימודים
+        <br />
+        שהוספת למועדפים
+      </div>
+      <Link href="/profile">
+        <a>
+          <Button className="h-[50px] w-[240px] mt-[30px]" status="secondary" name="לאזור האישי" />
+        </a>
+      </Link>
+      <Button
+        className="h-[50px] w-[240px] opacity-50 border-none font-bold bg-none hover:bg-none"
+        name="סגור"
+      />
     </div>
-    <Button className="h-[50px] w-[240px]" status="secondary" name="לאזור האישי" />
-    <Button className="h-[50px] w-[240px]" status="secondary" name="סגור" />
-  </div>
-);
+  );
+};
