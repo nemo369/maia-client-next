620dcc3014799d90706859e185c0d861e4083697
Mickey H <mickey@dicemarketing.co.il>
2021-07-26 11:45:23 +0300

    info

--- components/profile/ProfileInfo.js
+++ components/profile/ProfileInfo.js
@@ -1,5 +1,4 @@
-/* eslint-disable react/no-danger */
-/* eslint-disable operator-linebreak */
+/* eslint-disable prettier/prettier */
 import React, { useContext, useState, useEffect } from 'react';
 import Check from '../common/Check';
 import FemaleCrown from '../svg/FemaleCrown';
@@ -15,12 +14,9 @@ export default function ProfileInfo() {
   const user = { gender: 'f' };
   const { profile } = useContext(AppContext);
   const [lookingForJob, setLookingForJob] = useState(false);
-  const tooltipLookingForJob =
-    '<span>סגירת מצב ״מחפש עבודה״ תציג אותך במצב לא פעיל אצל<br /> המעסקים שאליהם שלחת בקשה והם לא יכולו לראות את <br /> פרטיך האישיים.</span>';
-  const tooltipSendedJobs =
-    '<span>סגירת מצב ״מחפש עבודה״ תציג אותך במצב לא פעיל אצל<br /> המעסקים שאליהם שלחת בקשה והם לא יכולו לראות את <br /> פרטיך האישיים.</span>';
-  const tooltipReachMe =
-    '<span><div> מעסיקים שמחפשים עובדים יוכלו לאתר אתכם דרך <br /> המאיה ולפנות אליכם בהצעות עבודה. אך אל דאגה!<br /> לא תהיה להם גישה אל קורות החיים שלכם ואל<br /> תוצאות האבחון ללא אישור מפורש מכם.</div><div style="color:#3c91a0; margin-top: 5px"><strong>מה כן יראה המעסיק?</strong></div><div><ul style="list-style: inside" ><li>את פרטי הקשר שלכם (שם מלא, מייל, נייד)</li><li> את אחוזי ההתאמה שלכם למשרה במקרים בהם <br /> אחוז ההתאמה עולה על 75%</li></ul></div></span>';
+  const tooltipLookingForJob = '<span>סגירת מצב ״מחפש עבודה״ תציג אותך במצב לא פעיל אצל<br /> המעסקים שאליהם שלחת בקשה והם לא יכולו לראות את <br /> פרטיך האישיים.</span>';
+  const tooltipSendedJobs = '<span>סגירת מצב ״מחפש עבודה״ תציג אותך במצב לא פעיל אצל<br /> המעסקים שאליהם שלחת בקשה והם לא יכולו לראות את <br /> פרטיך האישיים.</span>';
+  const tooltipReachMe = '<span><div> מעסיקים שמחפשים עובדים יוכלו לאתר אתכם דרך <br /> המאיה ולפנות אליכם בהצעות עבודה. אך אל דאגה!<br /> לא תהיה להם גישה אל קורות החיים שלכם ואל<br /> תוצאות האבחון ללא אישור מפורש מכם.</div><div style="color:#3c91a0; margin-top: 5px"><strong>מה כן יראה המעסיק?</strong></div><div><ul style="list-style: inside" ><li>את פרטי הקשר שלכם (שם מלא, מייל, נייד)</li><li> את אחוזי ההתאמה שלכם למשרה במקרים בהם <br /> אחוז ההתאמה עולה על 75%</li></ul></div></span>';
 
   console.log(profile);
   useEffect(() => {
