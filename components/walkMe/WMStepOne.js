133af2d6a00acd091e46e96ee36e1f3918273482
Mickey H <mickey@dicemarketing.co.il>
2021-09-01 16:32:37 +0300

    fixed style

--- components/walkMe/WMStepOne.js
+++ components/walkMe/WMStepOne.js
@@ -4,33 +4,43 @@ import ArrowOrange from '../svg/ArrowOrange';
 import WalkMeStepOne from '../svg/WalkMeStepOne';
 
 const WMStepOne = ({ closeModal, nextStep }) => (
-  <section className="w-[90%] ml-auto py-9 h-full mr-[180px]">
+  <>
     <DashboardHeader />
-    <div className="flex justify-center">
-      <div className="ml-[30px]">
-        <ArrowOrange />
-      </div>
-      <div className="text-[#FFFFFF] text-center">
-        <div className="w-[80px] mx-auto border-[1px] border-white rounded-[14px] py-[1px] px-[12px]">
-          1 מתוך 3
+    <section className="w-[90%] ml-auto py-9 h-full mr-[180px]">
+      <div className="flex justify-center">
+        <div className="ml-[30px]">
+          <ArrowOrange />
         </div>
-        <div className="text-[24px]">היכרות עם מערכת מאיה</div>
-        <div className="text-[28px] font-bold leading-[15px]">סולם התקדמות השלבים שלך</div>
-        <div className="text-[18px] mt-[20px]">
-          בראש המסך מופיע סרגל ההתקדמות שלך שיציג לך באיזה
-          <br />
-          שלב אתה נמצא ואלו שלבים עוד לפניך. ככל שתעברו יותר
-          <br />
-          שלבים, מאיה תציג בפניכם תוצאות מדויקות יותר
-          <br />
-          שמותאמות לכם
+        <div className="text-[#FFFFFF] text-center">
+          <div className="w-[80px] mx-auto border-[1px] border-white rounded-[14px] py-[1px] px-[12px]">
+            1 מתוך 3
+          </div>
+          <div className="text-[24px]">היכרות עם מערכת מאיה</div>
+          <div className="text-[28px] font-bold leading-[15px]">סולם התקדמות השלבים שלך</div>
+          <div className="text-[18px] mt-[20px] w-[380px]">
+            בראש המסך מופיע סרגל ההתקדמות שלך שיציג לך באיזה
+            <br />
+            שלב אתה נמצא ואלו שלבים עוד לפניך. ככל שתעברו יותר
+            <br />
+            שלבים, מאיה תציג בפניכם תוצאות מדויקות יותר
+            <br />
+            שמותאמות לכם
+          </div>
+          <div className="flex flex-col items-center">
+            <Button
+              name="הבא"
+              type="button"
+              status="main"
+              className="next-btn"
+              onClick={nextStep}
+            />
+            <Button name="דלג" type="button" className="skip-btn" onClick={closeModal} />
+          </div>
         </div>
-        <Button name="הבא" type="button" status="main" className="next-btn" onClick={nextStep} />
-        <Button name="דלג" type="button" className="skip-btn" onClick={closeModal} />
+        <WalkMeStepOne />
       </div>
-      <WalkMeStepOne />
-    </div>
-  </section>
+    </section>
+  </>
 );
 
 export default WMStepOne;
