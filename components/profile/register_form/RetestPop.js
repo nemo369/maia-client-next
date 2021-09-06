c22cc826f8265b352a19d3898ba5e30c179d1723
asi marom <asafmarom89@gmail.com>
2021-09-06 18:29:18 +0300

    added popups for test results-functionalty is still neede

---
+++ components/profile/register_form/RetestPop.js
@@ -0,0 +1,69 @@
+/* eslint-disable react/no-unescaped-entities */
+import { useState } from 'react';
+import Popup from 'reactjs-popup';
+import Image from 'next/image';
+import Xcircle from '../../svg/Xcircle';
+import Button from '../../common/Button';
+import warningsign from '../../../public/images/warningsign.png';
+
+const RetestPop = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Popup
+      modal
+      onClose={() => {
+        setOpen(!open);
+      }}
+      trigger={
+        <button
+          type="button"
+          className=""
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          <button
+            type="button"
+            className="text-3xl font-bold"
+            onClick={() => {
+              setOpen(!open);
+            }}
+          >
+            ope pop
+          </button>
+        </button>
+      }
+      //   nested
+    >
+      {(close) => (
+        <div className=" w-[500px] h-[500px] bg-white text-xl leading-5 text-gray-active overflow-hidden rounded-lg ">
+          <div className="w-[500px] h-[500px] bg-white grid px-1 pt-1 pb-5 rounded-md justify-items-center text-center gap-y-4">
+            <button
+              className="justify-self-start pr-2 pt-2"
+              type="button"
+              onClick={() => {
+                close();
+              }}
+            >
+              <Xcircle />
+            </button>
+            <Image src={warningsign} alt="מדליה" width={100} height={100} />
+            <h1 className="text-[32px] text-black font-black">ביצוע מבחן חוזר "מה מעניין אותי"</h1>
+            <p className="text-2xl px-5">האם אתה בטוח שברצונך לאפס את התוצאות שהתקבלו?</p>
+            <Button
+              className="w-auto px-3"
+              type="button"
+              status="secondary"
+              name="אפס תוצאות והתחל מחדש"
+            />
+            <button type="button" className="text-gray-active" onClick={() => close()}>
+              סגור
+            </button>
+          </div>
+        </div>
+      )}
+    </Popup>
+  );
+};
+export default RetestPop;
--- pages/user/signup.js
+++ pages/user/signup.js
@@ -2,9 +2,6 @@
 import { useTranslation } from 'next-i18next';
 import { NextSeo } from 'next-seo';
 import nookies from 'nookies';
-import Button from '../../components/common/Button';
-import StageResults from '../../components/common/stage1results/StageResults';
-import StagesPopSide from '../../components/common/StagesPopSide';
 import RegisterForm from '../../components/profile/RegisterForm';
 import SignUpHeader from '../../components/SignUpHeader';
 import { USER_COOKIE } from '../../src/utils/consts';
@@ -17,6 +14,7 @@ const Register = function (props) {
     title: t('הרשמה למערכת'),
   });
   const { cities, termsText } = props;
+
   return (
     <div
       className="bg-lightgreybackground w-full
@@ -25,15 +23,7 @@ const Register = function (props) {
       <NextSeo {...seo} />
       <SignUpHeader />
       <RegisterForm cities={cities} termsText={termsText} />
-      <StagesPopSide
-        trigger={
-          <Button type="button" status="secondary" name="תוצאות השלב" className="h-12 w-full" />
-        }
-      >
-        {/* <StageResults testType="CompletionIAmpro" /> */}
-        {/* <StageResults testType="Autobiography" /> */}
-        <StageResults testType="Veretas" />
-      </StagesPopSide>
+
     </div>
   );
 };
