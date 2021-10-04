ec7240ba0378809d77ea04bbf0e6e49316128867
naaman1 <nafrenkel@gmail.com>
2021-10-04 22:02:58 +0300

    try this

--- pages/api/user/login-with-email.js
+++ pages/api/user/login-with-email.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
-// import { setCookie, destroyCookie } from 'nookies';
-import cookie from 'cookie';
+import { setCookie, destroyCookie } from 'nookies';
 import { USER_COOKIE } from '../../../src/utils/consts';
 
 export default async function loginWithMail(req, res) {
   const { WORDPRESS_ENDPOINT, NODE_ENV } = process.env;
   const { method } = req;
-  // destroyCookie({ res }, USER_COOKIE);
+  destroyCookie({ res }, USER_COOKIE);
   const { email, password } = req.query;
 
   switch (method) {
@@ -23,22 +22,13 @@ export default async function loginWithMail(req, res) {
         if (!user) {
           throw new Error('No data :(');
         }
-        res.setHeader(
-          'Set-Cookie',
-          cookie.serialize(USER_COOKIE, JSON.stringify(user), {
-            httpOnly: true,
-            secure: 'development' !== NODE_ENV,
-            maxAge: 60 * 60,
-            sameSite: 'strict',
-            path: '/',
-          })
-        );
-        // setCookie({ res }, USER_COOKIE, JSON.stringify(user), {
-        //   secure: 'production' === NODE_ENV,
-        //   maxAge: 12 * 60 * 60, //12 hours as in Iam token
-        //   httpOnly: true,
-        //   path: '/',
-        // });
+
+        setCookie({ res }, USER_COOKIE, JSON.stringify(user), {
+          secure: 'production' === NODE_ENV,
+          maxAge: 12 * 60 * 60, //12 hours as in Iam token
+          httpOnly: true,
+          path: '/',
+        });
         res.status(200).json({ ...user });
       } catch (response) {
         res.status(response?.response.status ? response?.response.status : 502).json(response.data);
