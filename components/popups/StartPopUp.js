69da2b796eca503ccd9eb7f59b04d9bffc9e7127
Mickey H <mickey@dicemarketing.co.il>
2021-10-19 12:33:30 +0300

    walkme translation

--- components/popups/StartPopUp.js
+++ components/popups/StartPopUp.js
@@ -1,55 +1,44 @@
-import { useTranslation } from 'next-i18next';
-import React, { useContext } from 'react';
-import { AppContext } from '../../src/context/state';
-import { FRONT_URL } from '../../src/utils/consts';
+import React from 'react';
 import Button from '../common/Button';
 import PopUp from '../common/PopUp';
 import Logo from '../svg/Logo';
 
-const StartPopUp = () => (
+const StartPopUp = ({ nextStep }) => (
   <div>
     <PopUp defaultOpen>
-      <PopupContent />
+      <PopupContent nextStep={nextStep} />
     </PopUp>
   </div>
 );
 export default StartPopUp;
 
-const PopupContent = () => {
-  const { t } = useTranslation('common');
-  const { profile } = useContext(AppContext);
-  const onClick = (e) => {
-    e.preventDefault();
-    window.location.href = `${profile.vendor_token}&redirect=${encodeURIComponent(
-      `${FRONT_URL.replace('/api', '')}?refetchuser=true&testDone=autoBiography`
-    )}`;
-  };
-
-  return (
-    <div className="flex flex-col items-center justify-center px-16 text-center">
-      <div className="mb-4">
-        <Logo />
-      </div>
-      <h3 className="text-orange font-bold text-4xl">היי, כיף שבאתם!</h3>
-      <p className="mt-2 mb-8 max-w-[530px]">
-        אתם עומדים להתחיל תהליך אבחון שיעזור לכם בקבלת
-        <br />
-        ההחלטות הגדולות של הקריירה - כמו למשל מה ללמוד, במה
-        <br />
-        לעבוד ואיך להגיע לשם.
-        <br />
-        <br />
-        ככל שתתקדמו בתהליך ותעברו יותר שלבים, מאיה - היועצת
-        <br />
-        הדיגיטלית שלכם - תכיר אתכם טוב יותר ותציג בפניכם
-        <br />
-        תוצאות מדוייקות ומותאמות יותר עבורכם.
-        <br />
-        <strong className="font-bold">{t('והכי חשוב, אתם מתקדמים בקצב שלכם!!')}</strong>
-      </p>
-      <a onClick={onClick} href={profile?.vendor_token}>
-        <Button className="h-[50px] w-[240px]" status="gradient" name={t('אהבתי, בואו נתחיל')} />
-      </a>
+const PopupContent = ({ nextStep }) => (
+  <div className="flex flex-col items-center justify-center px-16 text-center">
+    <div className="mb-4">
+      <Logo />
     </div>
-  );
-};
+    <h3 className="text-orange font-bold text-4xl">היי, כיף שבאתם!</h3>
+    <p className="mt-2 mb-8 max-w-[550px] leading-5">
+      אתם עומדים להתחיל תהליך אבחון שיעזור לכם בקבלת
+      <br />
+      ההחלטות הגדולות של הקריירה - כמו למשל מה ללמוד, במה
+      <br />
+      לעבוד ואיך להגיע לשם.
+      <br />
+      <br />
+      ככל שתתקדמו בתהליך ותעברו יותר שלבים, מאיה - היועצת
+      <br />
+      הדיגיטלית שלכם - תכיר אתכם טוב יותר ותציג בפניכם
+      <br />
+      תוצאות מדוייקות ומותאמות יותר עבורכם.
+      <br />
+      <strong className="font-bold">{t('והכי חשוב, אתם מתקדמים בקצב שלכם!!')}</strong>
+    </p>
+    <Button
+      onClick={nextStep}
+      className="h-[45px] w-[210px]"
+      status="gradient"
+      name={t('אהבתי, בואו נתחיל')}
+    />
+  </div>
+);
